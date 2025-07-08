5ce9bc40416675a8f63fc67c850524cf7ff1c6e6
IrfanAhmed8 <jafriirfan37@gmail.com>
2025-07-09 00:59:52 +0530

    learn some basisc syntax and analysis steps for data analysis,such as info,shape,describe,isnull,drop

---
+++ analysis_manipulation.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+
+#load csv file for view purpose
+df=pd.read_csv("complete.csv")
+
+
+start_entries=df.head()
+end_entries=df.tail()
+column=df.columns
+# to get the idea about the data.
+#showed it contain 10 columns
+#print(start_entries)
+#print(end_entries)
+#print(column)
+
+
+
+#now i will try to understand data through various function
+
+#print(df.shape)
+#shape(4692,10)
+#print(df.info())
+#print(df.describe())
+#print(df['Name of State / UT'].value_counts())
+
+
+#print(df.isnull())
+#deleting column named as latitude,longitude.
+#df.drop(columns=['Latitude', 'Longitude'], inplace=True)
+
+state_data=df[df['Name of State / UT']=='Goa']
+print(state_data)
+print(state_data[['Date', 'New cases', 'New deaths']].tail())
+
+
+
+
